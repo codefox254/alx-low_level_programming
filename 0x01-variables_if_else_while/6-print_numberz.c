fc9597ab1793c2611fdc0ebbf42e1e00f8d98353
frank <frankode254.com>
2023-06-19 07:59:38 -0700

    adding the remaining tasks

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 0 to 9 and letters between a to f.
+ * main - Prints numbers from 0 to 9.
  *
  * Return: Always 0 (Success)
  */
@@ -13,11 +13,8 @@ int main(void)
 	{
 		putchar(i);
 	}
-	for (i = 97; i < 103; i++)
-	{
-		putchar(i);
-	}
 	putchar('\n');
 	return (0);
 }
+
 
