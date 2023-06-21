5ad186a314480ad7adfc79daed65dfa05cc8d499
frank <frankode254.com>
2023-06-21 08:18:06 -0700

    adding the 0x02-functions_nested_loops assignment

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int _putchar(char c) {
+    return putchar(c);
+}
+
+int main() {
+    
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    
+    return 0;
+}
