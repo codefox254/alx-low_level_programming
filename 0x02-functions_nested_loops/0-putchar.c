a561aefc0a4780722b316e50653e62b641580bad
frank <frankode254.com>
2023-06-21 08:38:23 -0700

    adding recent amendments

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,21 @@
 #include "main.h"
 
 /**
- * this program printes _putchar
+ * main - Program that prints _putchar
  * followed by a new line
- * should return 0 on sucess
+ * Return: 0 (Success)
  */
 
-int _putchar(char c) {
-    return putchar(c);
-}
-
-int main(void) {
-    
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    
-    return 0;
+int main(void)
+{
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }
