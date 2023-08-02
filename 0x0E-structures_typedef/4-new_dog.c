8761de101e8977fcc2eab28c9ebb0242b4c7fdd0
frank <frankode254.com>
2023-08-02 10:57:32 -0700

    adding amendment

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -83,3 +83,19 @@ dog_t *new_dog(char *name, float age, char *owner)
     return (dog);
 }
 
+/**
+ * free_dog - Frees memory allocated for a dog structure.
+ * @d: Pointer to the dog structure to free.
+ *
+ * Return: Void.
+ */
+void free_dog(dog_t *d)
+{
+    if (d != NULL)
+    {
+        free(d->name);
+        free(d->owner);
+        free(d);
+    }
+}
+
