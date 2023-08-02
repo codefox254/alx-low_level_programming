d83cc2f3fb4a0c3d800a528c3ebd5803aeed6078
frank <frankode254.com>
2023-08-02 12:26:58 -0700

    adding amendments

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,26 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * list_len - Counts the number of elements in a linked list.
- * @h: Pointer to the list_t list.
+ * print_list - Displays all elements of a linked list.
+ * @h: Pointer to the list_t list to display.
  *
- * Return: Number of elements in the list.
+ * Return: The number of nodes displayed.
  */
-size_t list_len(const list_t *h)
+size_t print_list(const list_t *h)
 {
-        size_t n = 0;
+	size_t s = 0;
 
-        while (h)
-        {
-                n++;
-                h = h->next;
-        }
-        return (n);
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+
+	return (s);
 }
 
