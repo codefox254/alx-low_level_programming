7d8f88d1253c1e7f6b11f459289d99d388ae7d18
codefox254 <frankode254@gmail.com>
2023-11-07 17:29:34 +0300

    Adding 0x0D-preprocessor assignment
    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the Name of the file
+ *
+ * Returns: Always 0 (Succ)
+ */
+int ain(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,2 @@
+This folder contains alx OXOD-PREPROCESSOR files including 0-object_like_macro.h, 1-pi.h
+2-main.c, 3-function_like_macro.h, 4-sum.h
