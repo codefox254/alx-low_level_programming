9ef08e0ed82ae30e387d302f4449bf7b45147783
codefox254 <frankode254@gmail.com>
2023-11-11 08:05:26 +0300

    array_iterator
    In this version, I added a more detailed description of the array_iterator function. Now, it should satisfy the Betty documentation style requirements.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,23 @@
 #include "function_pointers.h"
-	#include <stdio.h>
-	/**
-	 * array_iterator - print array elem onnew l
-	 * @array: list of array
-	 * @size: no ofelem to print
-	 * @action: pointer to print in regular or hex
-	 * Return: void
-	 */
-	void array_iterator(int *array, size_t size, void (*action)(int))
-	{
-		unsigned int i;
+#include <stdio.h>
 
+/**
+ * array_iterator - executes a given function on each element of an array
+ * @array: array to iterate over
+ * @size: size of the array
+ * @action: pointer to the function to apply to each element
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    unsigned int i;
 
-		if (array == NULL || action == NULL)
-			return;
+    if (array == NULL || action == NULL)
+        return;
 
-
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
-	}
+    for (i = 0; i < size; i++)
+    {
+        action(array[i]);
+    }
+}
