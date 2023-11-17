8235d4a16622d71319b3930ba25fd3340e6bc5ff
codefox254 <frankode254@gmail.com>
2023-11-17 16:50:08 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -3,8 +3,28 @@
 # Compile each .c file to a .o file
 gcc -Wall -pedantic -Werror -Wextra -c *.c
 
+# Check if compilation was successful
+if [ $? -ne 0 ]; then
+    echo "Compilation failed. Exiting."
+    exit 1
+fi
+
 # Create the static library liball.a
 ar -rc liball.a *.o
 
+# Check if library creation was successful
+if [ $? -ne 0 ]; then
+    echo "Library creation failed. Exiting."
+    exit 1
+fi
+
 # Index the library
 ranlib liball.a
+
+# Check if indexing was successful
+if [ $? -ne 0 ]; then
+    echo "Library indexing failed. Exiting."
+    exit 1
+fi
+
+echo "Script executed successfully."
