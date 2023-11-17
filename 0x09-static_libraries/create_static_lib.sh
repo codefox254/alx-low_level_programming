1591ad0b896c18ff97ec2bdecc6bab90fe12ab6f
codefox254 <frankode254@gmail.com>
2023-11-17 16:43:33 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -c *.c
+
+# Check if compilation was successful
+if [ $? -ne 0 ]; then
+    echo "Compilation failed"
+    exit 1
+else
+    echo "Compilation successful"
+fi
+
 ar -rc liball.a *.o
+
+# Check if ar was successful
+if [ $? -ne 0 ]; then
+    echo "Creating library failed"
+    exit 1
+else
+    echo "Library created successfully"
+fi
+
 ranlib liball.a
+
 
