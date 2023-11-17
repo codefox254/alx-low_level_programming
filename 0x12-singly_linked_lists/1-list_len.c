5a2084a858df7427feb9a8fada4259e842f1d1c5
codefox254 <frankode254@gmail.com>
2023-11-17 04:02:20 +0300

    update3

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,9 +3,10 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list_t list
- * @h: pointer to the head of the linked list
- * Return: number of elements in the list
+ * list_len - Returns the number of elements in a linked list_t list.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: Number of elements in the list.
  */
 size_t list_len(const list_t *h)
 {
