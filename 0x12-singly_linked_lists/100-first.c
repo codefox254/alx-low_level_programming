981c2d741d4c65a31bc0e01bb910505a51af2f5d
codefox254 <frankode254@gmail.com>
2023-11-18 13:36:10 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -14,7 +14,7 @@ list_t *add_node(list_t **head, const char *str)
     list_t *new;
     unsigned int len = 0;
 
-    while (str[len])
+    while (str && str[len])
         len++;
 
     new = malloc(sizeof(list_t));
@@ -22,6 +22,12 @@ list_t *add_node(list_t **head, const char *str)
         return (NULL);
 
     new->str = strdup(str);
+    if (!new->str)
+    {
+        free(new);
+        return (NULL);
+    }
+
     new->len = len;
     new->next = (*head);
     (*head) = new;
