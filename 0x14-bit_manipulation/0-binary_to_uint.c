1e524641d1bf4332cd995435f813e76a45fb1e93
codefox254 <frankode254@gmail.com>
2023-11-23 23:01:06 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+    unsigned int result = 0;
 
-	if (b == NULL)
-		return (0);
+    if (b == NULL)
+        return (0);
 
-	while (*b)
-	{
-		if (*b == '0' || *b == '1')
-		{
-			result = (result << 1) + (*b - '0');
-		}
-		else
-		{
-			return (0); /* Invalid character in the string */
-		}
+    while (*b)
+    {
+        if (*b == '0' || *b == '1')
+        {
+            result = (result << 1) + (*b - '0');
+        }
+        else
+        {
+            return (0); /* Invalid character in the string */
+        }
 
-		b++;
-	}
+        b++;
+    }
 
-	return (result);
+    return result;
 }
