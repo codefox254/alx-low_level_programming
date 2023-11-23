c9ea74af2286d9add3c1860838e57ca42b565e2c
codefox254 <frankode254@gmail.com>
2023-11-23 23:28:24 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * flip_bits - Returns the number of bits needed to flip to get from n to m
  * @n: First number
@@ -9,42 +11,12 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xor_result = n ^ m;
 	unsigned int count = 0;
+	int i;
 
-	while (xor_result)
+	for (i = 63; i >= 0; i--)
 	{
-		count += xor_result & 1;
-		xor_result >>= 1;
+		count += (xor_result >> i) & 1;
 	}
 
 	return count;
-}
-
-/**
- * binary_to_uint - Converts a binary number to an unsigned int
- * @b: Pointer to a string of 0 and 1 chars
- *
- * Return: The converted number, or 0 if there is an invalid character
- */
-unsigned int binary_to_uint(const char *b)
-{
-	unsigned int result = 0;
-
-	if (b == NULL)
-		return (0);
-
-	while (*b)
-	{
-		if (*b == '0' || *b == '1')
-		{
-			result = (result << 1) + (*b - '0');
-		}
-		else
-		{
-			return (0); /* Invalid character in the string */
-		}
-
-		b++;
-	}
-
-	return result;
 }
