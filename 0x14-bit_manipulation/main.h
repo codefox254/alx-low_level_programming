86670927031a3ed5af524702e93dfb0eb89eb6ba
codefox254 <frankode254@gmail.com>
2023-11-23 22:58:26 +0300

    update2

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* binary_to_uint.c */
 unsigned int binary_to_uint(const char *b);
-
-/* print_binary.c */
 void print_binary(unsigned long int n);
 int _putchar(char c);
 
-/* get_bit.c */
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif /* MAIN_H */
