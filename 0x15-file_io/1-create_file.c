a95360b03f249b6c8338304918674b82f4df31fd
codefox254 <frankode254@gmail.com>
2023-11-27 23:46:29 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,16 @@
-#include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
 
 /**
- * create_file - Creates a file and writes text content to it
- * @filename: The name of the file to create
- * @text_content: The NULL terminated string to write to the file
+ * create_file - Creates a file and writes text content to it.
+ * @filename: The name of the file to be created.
+ * @text_content: The content to write to the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure.
+ *
+ * Description: The file is created with read and write permissions only
+ * for the owner (0600). If filename is NULL or if an error occurs during
+ * file creation or writing, -1 is returned.
  */
 int create_file(const char *filename, char *text_content)
 {
