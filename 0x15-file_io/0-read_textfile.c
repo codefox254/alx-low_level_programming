767d671cd2ae46ae97f89417411ad4b58da9f97b
codefox254 <frankode254@gmail.com>
2023-11-27 15:58:23 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,30 +11,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	int num_letters;
-	int write_result;
+	int fd;
+	int n;
+	int wr;
 
 	if (!filename)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (file_descriptor == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (num_letters = 0; text_content[num_letters]; num_letters++)
+	for (n = 0; text_content[n]; n++)
 		;
 
-	write_result = write(file_descriptor, text_content, num_letters);
+	wr = write(fd, text_content, n);
 
-	if (write_result == -1)
+	if (wr == -1)
 		return (-1);
 
-	close(file_descriptor);
+	close(fd);
 
 	return (1);
 }
