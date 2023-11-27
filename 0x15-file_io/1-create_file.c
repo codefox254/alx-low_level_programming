4a355aa7191195e1f7df282289b200eb66f2b4fc
codefox254 <frankode254@gmail.com>
2023-11-27 23:19:38 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,27 +11,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor, write_result;
-    ssize_t len = 0;
+	int fd, w, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (file_descriptor == -1)
-        return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-        write_result = write(file_descriptor, text_content, len);
-        close(file_descriptor);
+	if (fd == -1 || w == -1)
+		return (-1);
 
-        if (write_result == -1 || (size_t)write_result != len)
-            return (-1);
-    }
+	close(fd);
 
-    return (1);
+	return (1);
 }
