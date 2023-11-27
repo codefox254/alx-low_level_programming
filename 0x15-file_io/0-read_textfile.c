4285dac9f16286f8b73553795282b639d1d85506
codefox254 <frankode254@gmail.com>
2023-11-27 16:03:39 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
 /**
  * create_file - Creates a file with specified content
@@ -11,30 +12,40 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+    int fd;
+    int nletters;
+    int rwr;
 
-	if (!filename)
-		return (-1);
+    if (!filename)
+    {
+        perror("Error: Invalid filename");
+        return (-1);
+    }
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
-		return (-1);
+    if (fd == -1)
+    {
+        perror("Error opening file");
+        return (-1);
+    }
 
-	if (!text_content)
-		text_content = "";
+    if (!text_content)
+        text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
+    for (nletters = 0; text_content[nletters]; nletters++)
+        ;
 
-	rwr = write(fd, text_content, nletters);
+    rwr = write(fd, text_content, nletters);
 
-	if (rwr == -1)
-		return (-1);
+    if (rwr == -1)
+    {
+        perror("Error writing to file");
+        close(fd);
+        return (-1);
+    }
 
-	close(fd);
+    close(fd);
 
-	return (1);
+    return (1);
 }
