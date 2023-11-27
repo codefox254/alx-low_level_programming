bca07a2c93b01c7eaa2d6e6d2b2bd6d9d940e6d1
codefox254 <frankode254@gmail.com>
2023-11-27 15:29:19 +0300

    update2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * create_file - Creates a file and writes text content to it
@@ -9,27 +11,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, write_result;
-	ssize_t len = 0;
+    int file_descriptor, write_result;
+    ssize_t len = 0;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file_descriptor == -1)
-		return (-1);
+    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if (file_descriptor == -1)
+        return (-1);
 
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
+    if (text_content != NULL)
+    {
+        while (text_content[len])
+            len++;
 
-		write_result = write(file_descriptor, text_content, len);
-		close(file_descriptor);
+        write_result = write(file_descriptor, text_content, len);
+        close(file_descriptor);
 
-		if (write_result == -1 || (size_t)write_result != len)
-			return (-1);
-	}
+        if (write_result == -1 || (size_t)write_result != len)
+            return (-1);
+    }
 
-	return (1);
+    return (1);
 }
