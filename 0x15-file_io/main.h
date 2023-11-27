257e7ba04f41211ee35671fe6eaa11c27b74ede7
codefox254 <frankode254@gmail.com>
2023-11-27 23:06:45 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,13 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <elf.h>
+#include <unistd.h>
 
-void display_elf_header(const char *filename);
-int read_elf_header(int fd, Elf64_Ehdr *elf_header);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
