81f5dc1c1ac05e568f548432c7f0fcd811e50a20
codefox254 <frankode254@gmail.com>
2023-11-27 23:52:55 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,5 +8,6 @@
 void display_error_and_exit(const char *error_message);
 void display_elf_header(const char *filename);
 int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
