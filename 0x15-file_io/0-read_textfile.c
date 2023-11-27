42340d6843cb328a141e5a927999af50b9827356
codefox254 <frankode254@gmail.com>
2023-11-27 15:37:17 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,41 @@
 #include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - Reads a text file and prints it to the POSIX standard output
  * @filename: The name of the file to read
- * @letters: The number of letters to read and print
+ * @num_letters: The number of letters to read and print
  *
  * Return: The actual number of letters read and printed; 0 on failure
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t num_letters)
 {
-    int file_descriptor, read_result;
-    char buffer[1024];
+    int file_descriptor;
+    ssize_t num_read, num_written;
+    char *buffer;
 
-    if (filename == NULL)
+    if (!filename)
         return (0);
 
     file_descriptor = open(filename, O_RDONLY);
+
     if (file_descriptor == -1)
         return (0);
 
-    read_result = read(file_descriptor, buffer, sizeof(buffer));
-    close(file_descriptor);
-
-    if (read_result == -1)
+    buffer = malloc(sizeof(char) * num_letters);
+    if (!buffer)
+    {
+        close(file_descriptor);
         return (0);
+    }
 
-    write(STDOUT_FILENO, buffer, read_result);
+    num_read = read(file_descriptor, buffer, num_letters);
+    num_written = write(STDOUT_FILENO, buffer, num_read);
 
-    return (read_result);
+    close(file_descriptor);
+    free(buffer);
+
+    return (num_written);
 }
