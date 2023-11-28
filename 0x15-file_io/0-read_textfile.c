e352fbc003dee37685ea7c7f67a234ba39e08059
codefox254 <frankode254@gmail.com>
2023-11-28 09:22:35 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,37 +15,37 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd, w, t;
+    char *buf;
+    ssize_t fd, w, t;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+    buf = malloc(sizeof(char) * letters);
+    if (buf == NULL)
+    {
+        close(fd);
+        return (0);
+    }
 
-	t = read(fd, buf, letters);
-	if (t == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+    t = read(fd, buf, letters);
+    if (t == -1)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
 
-	w = write(STDOUT_FILENO, buf, t);
-	if (w == -1 || w != t)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+    w = write(STDOUT_FILENO, buf, t);
+    if (w == -1 || w != t)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
 
-	free(buf);
-	close(fd);
-	return (w);
+    free(buf);
+    close(fd);
+    return (w);
 }
