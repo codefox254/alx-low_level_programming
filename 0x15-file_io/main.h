a7321b4044d7acbc28a357411a865ccdf92294d9
codefox254 <frankode254@gmail.com>
2023-11-28 15:59:44 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void display_error_and_exit(const char *error_message);
+void display_error_and_exit(const char *error_message) {
+    perror(error_message);
+    exit(EXIT_FAILURE);
+}
+
 void display_elf_header(const char *filename);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
