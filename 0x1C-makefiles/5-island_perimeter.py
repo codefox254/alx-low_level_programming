1635bc6afc511e846d49f552cf83a53b86612c21
frank <frankode254@gmail.com>
2024-03-20 23:32:33 +0300

    adding makefile python files

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -19,16 +19,17 @@ def island_perimeter(grid):
         - Grid is completely surrounded by water, and there is one island (or nothing).
         - The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
     """
-    perimeter = 0
-    rows, cols = len(grid), len(grid[0])
+width = len(grid[0])
+height = len(grid)
+edges = 0
+size = 0
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-
-    return perimeter
+for row_index in range(height):
+    for column_index in range(width):
+        if grid[row_index][column_index] == 1:
+            size += 1
+            if (column_index > 0 and grid[row_index][column_index - 1] == 1):
+                edges += 1
+            if (row_index > 0 and grid[row_index - 1][column_index] == 1):
+                edges += 1
+return size * 4 - edges * 2
